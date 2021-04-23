8ba017ff322964d9c5592978a4d596f99e74fc2c
mikle006 <kurch7815@gmail.com>
2021-04-23 08:54:16 +0300

    1commit
    Я надеюсь что это сработает

---
+++ views/site/carousel.php
@@ -0,0 +1,38 @@
+<div id="carousel-example" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example" data-slide-to="1"></li>
+    <li data-target="#carousel-example" data-slide-to="2"></li>
+  </ol>
+
+  <div class="carousel-inner">
+    <div class="item active">
+      <a href="#"><img src="https://sun9-67.userapi.com/c857720/v857720229/16ade8/J31o-5XRhR8.jpg" /></a>
+      <div class="carousel-caption">
+        <h3>Meow</h3>
+        <p>Just Kitten Around</p>
+      </div>
+    </div>
+    <div class="item">
+      <a href="#"><img src="https://sun9-9.userapi.com/c857720/v857720229/16ae1a/jKXuVyBn3dY.jpg" /></a>
+      <div class="carousel-caption">
+        <h3>Meow</h3>
+        <p>Just Kitten Around</p>
+      </div>
+    </div>
+    <div class="item">
+      <a href="#"><img src="http://placekitten.com/1600/600" /></a>
+      <div class="carousel-caption">
+        <h3>Meow</h3>
+        <p>Just Kitten Around</p>
+      </div>
+    </div>
+  </div>
+
+  <a class="left carousel-control" href="#carousel-example" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
---
+++ views/site/index.php
@@ -0,0 +1,51 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Paid medicine is available to everyone!</h1>
+
+        <p class="lead">The first examination at an affordable price and get your own course from the best doctors of St. Petersburg!</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://yii/web/site/form">Go to prices now</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Congratulations!</h2>
+
+                <p>Happy Great Victory Day to our great nation! All retirees and those who were before the reforms in 2077 
+                   (order from the putibot#3000) for three days and for three days after the day of the GREAT VICTORY you can 
+                   get a 30% discount on prostate treatment services. Hurry write, places are limited!</p>
+
+                <p><a class="btn btn-default" href="/site/doctors">Check out price &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>News</h2>
+
+                <p>Great news! We noticed that many patients complain about the inconvenience of the recording system on 
+                   the old site design and finally the best programmers of St. Petersburg Radio Technical College (RTK) have 
+                   updated the site design.
+                   I`m waiting for your feedback and please give a credit in practice. I really want a scholarship.</p>
+
+                <p><a class="btn btn-default" href="https://mikle006.github.io/DeathWave/redir.html">His own contact &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Soon</h2>
+
+                <p>
+                In the very near future, we plan to change the domain or go into the underground market. But for now, you can sign up for one of our doctors.
+                </p>
+
+                <p><a class="btn btn-default" href="/site/contact">Pre-registrate &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
